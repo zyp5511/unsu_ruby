304f7ee3d0f491601a290c98b73980c087e85796
Lichao Chen <lichao_chen@outlook.com>
2015-07-06 10:38:15 -0700

    modify extract_all_pairs.rb to the new record.rb interface

--- extract_all_pairs.rb
+++ extract_all_pairs.rb
@@ -8,7 +8,7 @@ require_relative 'transform.rb'
 lcdat = ARGV[0]
 listfn = ARGV[1]
 File.open(listfn,'w') do |f|
-	Record::seperate_records("","",IO.foreach(lcdat)).select{|r| r.rects!=nil}.each do |r|
+	Record::seperate_records("",IO.foreach(lcdat),Record::parsers[:origin]).select{|r|r.rects!=nil}.each do |r|
 		len = r.rects.length
 		(0...(len-1)).each do |i|
 			((i+1)...(len)).each do |j|
